e57e3411bc024b48f498a9bb79cfec140bbeff74
Dangi83 <dangiamit83@gmail.com>
2022-10-13 12:44:00 +0530

    My first Code.

---
+++ .vscode/stdio.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c, d, e ,f ;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number ");
+    scanf("%d", &b);
+    printf("Enter the third number ");
+    scanf("%d", &b);
+    printf("Enter the fourth number ");
+    scanf("%d", &d);
+    printf("Enter the fifth number ");
+    scanf("%d", &e);
+    int output =(a*b)+c+d+e ;
+    printf("the output is %d", output );
+    return 0;
+}
